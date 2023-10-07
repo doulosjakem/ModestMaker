b4734333b03dd415f96b5e2b3bcbfd6f2da23072
Jake Miller <doulosjakem@gmail.com>
2023-10-06 18:30:22 -0600

    first commit

---
+++ BotFooter.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="PPgreen  text-center mb-0">
+        <div class="py-2">
+            <a href="./index.php">
+                <img class="rounded img-fluid my-2" src="images/paisanos-place-icon.jpg"
+                    alt="chef in front of italian flag">
+            </a>
+        </div>
+        <div class="">
+            <div class="col">
+                <a href="tel:000-000-0000" class="PPbluetxt text-decoration-none">
+                    <h4 class="fs-6">Phone: (000)-000-0000</h4>
+                </a>
+            </div>
+            <div class="col">
+                <a href="mailto:PaisanosPlace@gmail.com" class="PPbluetxt text-decoration-none">
+                    <h4 class="fs-6">Email: PaisanosPlace@gmail.com</h4>
+                </a>
+            </div>
+            <div class="col">
+                <a href="./location.php" class="PPbluetxt text-decoration-none">
+                    <h4 class="fs-6">Address: 300 N. South St. Boise, ID</h4>
+                </a>
+            </div>
+        </div>
+        <p class="PPblacktxt">&copy
+            <?php echo date("Y"); ?> Paisano's Place. All Rights Reserved.
+        </p>
+    </div>
+</footer>
